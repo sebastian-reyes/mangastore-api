45f117108fa9da127d287e07d2e4a7daa1ccbb42
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2022-02-14 23:33:36 -0500

    Creando proyecto, inicializando repositorio

---
+++ src/test/java/com/manga/api/ApiMangaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manga.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMangaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
